60f760a6c63f499f7c16942e6203ef06d1dc99c0
Marko <stankovicmarko333@gmail.com>
2020-06-10 20:52:20 +0200

    Kancelarija10.06.2020

---
+++ wwwroot/scripts/izvestaj.js
@@ -0,0 +1,40 @@
+
+
+
+
+let body = document.getElementById("tBody");
+
+let tr = document.createElement("tr");
+body.appendChild(tr);
+
+let td1 = document.createElement("td");
+tr.appendChild(td1);
+td1.textContent = "dat od";
+td1.className = "thTable";
+
+let td2 = document.createElement("td");
+tr.appendChild(td2);
+td2.textContent = "dat do";
+td2.className = "thTable";
+
+let td3 = document.createElement("td");
+tr.appendChild(td3);
+td3.textContent = "Ime i prez";
+td3.className = "thTable";
+
+let td4 = document.createElement("td");
+tr.appendChild(td4);
+td4.textContent = "dat od";
+td4.className = "thTable";
+
+let td5 = document.createElement("td");
+tr.appendChild(td5);
+td5.textContent = "dat od";
+td5.className = "thTable";
+
+let td6 = document.createElement("td");
+tr.appendChild(td6);
+td6.textContent = "dat od";
+td6.className = "thTable";
+
+
